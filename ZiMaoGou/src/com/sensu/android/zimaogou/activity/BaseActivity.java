aeba3a1bc0f520b4ad98fb227be2a0fe6dda0d4b
wentao_zhang <personal_works@163.com>
2015-11-15 21:21:26 +0800

    [added] - 设置首页NoTitleBar

--- ZiMaoGou/src/com/sensu/android/zimaogou/activity/BaseActivity.java
+++ ZiMaoGou/src/com/sensu/android/zimaogou/activity/BaseActivity.java
@@ -25,10 +25,10 @@ public class BaseActivity extends Activity {
          * android:fitsSystemWindows="true"
          */
 
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-//            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-//        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
     }
 
     @Override
