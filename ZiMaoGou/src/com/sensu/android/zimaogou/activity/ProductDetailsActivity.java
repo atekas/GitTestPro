5fda6305d0b9284910c5c6b5ad6e9ab8862effc9
winter <personal_works@163.com>
2015-11-20 18:04:37 +0800

    [added] - 添加商品详情页

---
+++ ZiMaoGou/src/com/sensu/android/zimaogou/activity/ProductDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.sensu.android.zimaogou.activity;
+
+import android.os.Bundle;
+import com.sensu.android.zimaogou.R;
+
+/**
+ * Created by zhangwentao on 2015/11/20.
+ */
+public class ProductDetailsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_details_activity);
+    }
+}
