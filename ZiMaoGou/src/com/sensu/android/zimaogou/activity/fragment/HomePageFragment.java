00f77164684a0348b7f012fc2b7212d522f0e9fc
winter <personal_works@163.com>
2015-11-26 12:06:53 +0800

    [added] - 添加首页轮播功能

--- ZiMaoGou/src/com/sensu/android/zimaogou/activity/fragment/HomePageFragment.java
+++ ZiMaoGou/src/com/sensu/android/zimaogou/activity/fragment/HomePageFragment.java
@@ -8,6 +8,9 @@ import com.sensu.android.zimaogou.R;
 import com.sensu.android.zimaogou.activity_home.HomeHorizontalLinearLayout;
 import com.sensu.android.zimaogou.activity_home.HomeVerticalLinearLayout;
 import com.sensu.android.zimaogou.utils.PromptUtils;
+import com.sensu.android.zimaogou.widget.ImageRollView;
+
+import java.util.ArrayList;
 
 /**
  * Created by zhangwentao on 2015/11/10.
@@ -22,8 +25,10 @@ public class HomePageFragment extends BaseFragment implements View.OnClickListen
     //推荐单品
     public static int RECOMMEND_ITEM_TYPE = 5;
 
-    HomeHorizontalLinearLayout mDailyLinearLayout,mGroupLinearLayout,mFindStoreLinearLayout;
+    HomeHorizontalLinearLayout mDailyLinearLayout, mGroupLinearLayout, mFindStoreLinearLayout;
     HomeVerticalLinearLayout mLivelyLinearLayout, mRecommendItemLayout;
+
+    private ImageRollView mImageRollView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -62,6 +67,17 @@ public class HomePageFragment extends BaseFragment implements View.OnClickListen
         mFindStoreLinearLayout.setData(FIND_STORE_TYPE);
         mLivelyLinearLayout.setData(LIVELY_STORE_TYPE);
         mRecommendItemLayout.setData(RECOMMEND_ITEM_TYPE);
+
+        mImageRollView = (ImageRollView) mParentActivity.findViewById(R.id.banner);
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("http://img.taodiantong.cn/v55183/infoimg/2013-07/130720115322ky.jpg");
+        list.add("http://pic30.nipic.com/20130626/8174275_085522448172_2.jpg");
+        list.add("http://pic18.nipic.com/20111215/577405_080531548148_2.jpg");
+        list.add("http://pic15.nipic.com/20110722/2912365_092519919000_2.jpg");
+        list.add("http://pic.58pic.com/58pic/12/64/27/55U58PICrdX.jpg");
+
+        mImageRollView.initImageRollView(list, true);
 
         mParentActivity.findViewById(R.id.ll_buyRead).setOnClickListener(this);
         mParentActivity.findViewById(R.id.ll_hotGoods).setOnClickListener(this);
