551c8ab052c3bb905a71bb0808b6bdf5c2a72d62
qi.yang <qi.yang@sensu-sh.com>
2015-12-04 15:07:21 +0800

    [added] 添加游购中 评论功能

--- ZiMaoGou/src/com/sensu/android/zimaogou/activity/tour/TourBuyDetailsActivity.java
+++ ZiMaoGou/src/com/sensu/android/zimaogou/activity/tour/TourBuyDetailsActivity.java
@@ -3,7 +3,9 @@ package com.sensu.android.zimaogou.activity.tour;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import com.sensu.android.zimaogou.R;
 import com.sensu.android.zimaogou.activity.BaseActivity;
 import com.sensu.android.zimaogou.external.umeng.share.UmengShare;
@@ -19,7 +21,8 @@ public class TourBuyDetailsActivity extends BaseActivity implements View.OnClick
     private UmengShare mUmengShare;
 
     private View mHeaderView;
-
+    private RelativeLayout mBottomRelativeLayout;
+    private Button mCommentSureButton,mCloseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,16 +35,41 @@ public class TourBuyDetailsActivity extends BaseActivity implements View.OnClick
     private void initViews() {
         mUmengShare = UmengShare.getInstance(this);
         mTourDetailsListView = (ListView) findViewById(R.id.review_details);
+        mBottomRelativeLayout = (RelativeLayout) findViewById(R.id.rl_bottom);
+        mCommentSureButton = (Button) findViewById(R.id.bt_sure);
+        mCloseButton = (Button) findViewById(R.id.bt_close);
         mHeaderView = LayoutInflater.from(this).inflate(R.layout.tour_details_header, null);
         mTourDetailsListView.addHeaderView(mHeaderView);
         mTourBuyDetailsAdapter = new TourBuyDetailsAdapter(this);
+
         mTourDetailsListView.setAdapter(mTourBuyDetailsAdapter);
 
         findViewById(R.id.back).setOnClickListener(this);
         findViewById(R.id.share).setOnClickListener(this);
 
+        mCommentSureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBottomRelativeLayout.setVisibility(View.GONE);
+            }
+        });
+        mCloseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBottomRelativeLayout.setVisibility(View.GONE);
+            }
+        });
         initHeader();
     }
+
+    /**
+     * 评论
+     * @param v
+     */
+    public void CommentClick(View v){
+        mBottomRelativeLayout.setVisibility(View.VISIBLE);
+    }
+
 
     private void initHeader() {
 
