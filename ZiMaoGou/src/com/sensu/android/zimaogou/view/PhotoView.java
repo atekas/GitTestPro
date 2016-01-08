eac40abdd71ed747ee92e65f4ebd3e322987021e
winter <personal_works@163.com>
2016-01-08 17:57:32 +0800

    [updated] - 添加商品详情占位图

--- ZiMaoGou/src/com/sensu/android/zimaogou/view/PhotoView.java
+++ ZiMaoGou/src/com/sensu/android/zimaogou/view/PhotoView.java
@@ -2,10 +2,13 @@ package com.sensu.android.zimaogou.view;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.sensu.android.zimaogou.R;
 import com.sensu.android.zimaogou.activity.ShowImageActivity;
 import com.sensu.android.zimaogou.utils.ImageUtils;
@@ -22,6 +25,19 @@ public class PhotoView extends FrameLayout implements View.OnClickListener {
         super(context, attrs);
     }
 
+    public DisplayImageOptions mProductDetailOptions = new DisplayImageOptions.Builder()
+            .considerExifParams(true)
+            .imageScaleType(ImageScaleType.EXACTLY)
+            .bitmapConfig(Bitmap.Config.RGB_565)
+            .resetViewBeforeLoading(true)
+            .showImageOnLoading(R.drawable.product_details_default)
+            .showImageOnFail(R.drawable.product_details_default)
+            .showImageForEmptyUri(R.drawable.product_details_default)
+            .cacheInMemory(true)
+            .resetViewBeforeLoading(true)
+            .cacheOnDisk(true)
+            .build();
+
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
@@ -31,7 +47,7 @@ public class PhotoView extends FrameLayout implements View.OnClickListener {
 
     public void setPhotoData(String imagePath, boolean isVideo, String videoPath) {
         mImageUrl = imagePath;
-        ImageUtils.displayImage(imagePath, ((ImageView) findViewById(R.id.photo)));
+        ImageUtils.displayImage(imagePath, ((ImageView) findViewById(R.id.photo)), mProductDetailOptions);
         //todo 有数据时进行逻辑处理
         if (isVideo) {
             findViewById(R.id.video_play).setVisibility(VISIBLE);
