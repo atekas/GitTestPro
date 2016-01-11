75c1b275f8015d88419cbca6b6e015548cd0d28c
winter <personal_works@163.com>
2016-01-11 15:30:36 +0800

    [updated] - 修改支付结果页面

--- ZiMaoGou/src/com/sensu/android/zimaogou/activity/PayResultActivity.java
+++ ZiMaoGou/src/com/sensu/android/zimaogou/activity/PayResultActivity.java
@@ -3,13 +3,16 @@ package com.sensu.android.zimaogou.activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import com.sensu.android.zimaogou.IConstants;
 import com.sensu.android.zimaogou.R;
+import com.sensu.android.zimaogou.utils.PromptUtils;
 
 /**
  * Created by zhangwentao on 2016/1/4.
  */
-public class PayResultActivity extends BaseActivity {
+public class PayResultActivity extends BaseActivity implements View.OnClickListener {
 
     public static final String ORDER_DATA = "order_data";
     public static final int PAYMENT_REQUEST_CODE = 1000;
@@ -25,6 +28,9 @@ public class PayResultActivity extends BaseActivity {
     }
 
     private void initViews() {
+
+        findViewById(R.id.back).setOnClickListener(this);
+
         mPayInfo = getIntent().getStringExtra(ORDER_DATA);
         if (mPayInfo != null) {
             Intent intent = new Intent();
@@ -33,6 +39,44 @@ public class PayResultActivity extends BaseActivity {
             intent.setComponent(componentName);
             intent.putExtra(IConstants.EXTRA_CHARGE, mPayInfo);
             startActivityForResult(intent, PAYMENT_REQUEST_CODE);
+        } else {
+            ((TextView) findViewById(R.id.pay_result_title)).setText("支付失败");
+            findViewById(R.id.pay_failed_layout).setVisibility(View.VISIBLE);
+            findViewById(R.id.pay_success_layout).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            ((TextView) findViewById(R.id.pay_result_title)).setText("支付失败");
+            findViewById(R.id.pay_failed_layout).setVisibility(View.VISIBLE);
+            findViewById(R.id.pay_success_layout).setVisibility(View.GONE);
+            return;
+        }
+        switch (requestCode) {
+            case PAYMENT_REQUEST_CODE:
+                String result = data.getExtras().getString("pay_result");
+                /* 处理返回值
+                 * "success" - payment succeed
+                 * "fail"    - payment failed
+                 * "cancel"  - user canceld
+                 * "invalid" - payment plugin not installed
+                 */
+                String errorMsg = data.getExtras().getString("error_msg"); // 错误信息
+                String extraMsg = data.getExtras().getString("extra_msg"); // 错误信息
+                PromptUtils.showToast("支付返回：" + result + "  " + errorMsg + "  " + extraMsg);
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.back:
+                finish();
+                break;
         }
     }
 }
