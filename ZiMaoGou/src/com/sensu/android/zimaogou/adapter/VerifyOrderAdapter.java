6bd29a3d516a205fac99f40c9076668b80d5f7f4
winter <personal_works@163.com>
2016-01-11 10:25:07 +0800

    [updated] - 修改专题图片的尺寸大小,并重新修改确认订单页面的界面

--- ZiMaoGou/src/com/sensu/android/zimaogou/adapter/VerifyOrderAdapter.java
+++ ZiMaoGou/src/com/sensu/android/zimaogou/adapter/VerifyOrderAdapter.java
@@ -42,15 +42,22 @@ public class VerifyOrderAdapter extends SimpleBaseAdapter {
             viewHolder.mProductSize = (TextView) view.findViewById(R.id.product_size);
             viewHolder.mNum = (TextView) view.findViewById(R.id.product_count);
             viewHolder.mPrice = (TextView) view.findViewById(R.id.product_price);
+            viewHolder.mBottomLine = view.findViewById(R.id.bottom_line);
             view.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) view.getTag();
         }
 
+        if (i == (getCount() - 1)) {
+            viewHolder.mBottomLine.setVisibility(View.GONE);
+        } else {
+            viewHolder.mBottomLine.setVisibility(View.VISIBLE);
+        }
+
         SelectProductModel.GoodsInfo goodsInfo = mSelectProductModel.getGoodsInfo().get(i);
         viewHolder.mProductName.setText(goodsInfo.getName());
         viewHolder.mPrice.setText(goodsInfo.getPrice());
-        viewHolder.mNum.setText(goodsInfo.getNum());
+        viewHolder.mNum.setText("×" + goodsInfo.getNum());
         viewHolder.mProductSize.setText(goodsInfo.getSpec());
         ImageUtils.displayImage(goodsInfo.getImage(), viewHolder.mProductImage);
 
@@ -63,5 +70,6 @@ public class VerifyOrderAdapter extends SimpleBaseAdapter {
         TextView mProductSize;
         TextView mNum;
         TextView mPrice;
+        View mBottomLine;
     }
 }
